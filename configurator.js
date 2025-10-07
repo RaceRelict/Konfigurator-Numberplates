f7ef156565e128fb56cda3dd9c8ab875e6314570
RaceRelict <racerelict@gmail.com>
2025-10-07 13:17:37 +0200

    Update configurator.js

--- configurator.js
+++ configurator.js
@@ -1,5 +1,1 @@
-function showBrand(brand) {
-  const configurator = document.getElementById('configurator');
-  configurator.innerHTML = `<h2>${brand.toUpperCase()} Konfigurator</h2>
-    <p>Hier kannst du dein Logo auf die Mini Plate platzieren.</p>`;
-}
+
